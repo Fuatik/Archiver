df8436f27385261cd1de60c8c379605d548c1b36
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-02-22 10:52:43 +0100

    Added method createZip

--- ZipFileManager.java
+++ ZipFileManager.java
@@ -1,9 +1,9 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
@@ -17,9 +17,9 @@ public class ZipFileManager {
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
             ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
             zipOutputStream.putNextEntry(zipEntry);
-            byte[] buffer = new byte[8 * 1021];
-            int len;
-            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(source))) {
+            try (InputStream zipInputStream = Files.newInputStream(source)) {
+                byte[] buffer = new byte[8 * 1021];
+                int len;
                 while ((len = zipInputStream.read(buffer)) > 0) {
                     zipOutputStream.write(buffer, 0, len);
                 }
