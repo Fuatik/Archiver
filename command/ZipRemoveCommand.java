941148f67a30ddb26d2ddc69f2a2236f53c21918
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-02-22 23:21:38 +0100

    Added a ZipAddCommand. Refactoring

--- command/ZipRemoveCommand.java
+++ command/ZipRemoveCommand.java
@@ -6,17 +6,17 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class ZipRemoveCommand extends ZipCommand{
+public class ZipRemoveCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         ConsoleHelper.writeMessage("Удаление файла из архива.");
 
         ZipFileManager zipFileManager = getZipFileManager();
 
-        ConsoleHelper.writeMessage("Введите полный путь файла для удаления:");
+        ConsoleHelper.writeMessage("Введите полный путь файла в архиве:");
         Path sourcePath = Paths.get(ConsoleHelper.readString());
         zipFileManager.removeFile(sourcePath);
 
-        ConsoleHelper.writeMessage("Файл был удален из архива.");
+        ConsoleHelper.writeMessage("Удаление из архива завершено.");
     }
 }
