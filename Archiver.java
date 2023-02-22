12da9a26e9ed6113cde3b6e86251999ba164041e
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-02-22 10:48:32 +0100

    Added method createZip

--- Archiver.java
+++ Archiver.java
@@ -2,20 +2,16 @@ package com.javarush.task.task31.task3110;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.println("Введите полный путь к файлу архива, включая имя файла");
-            Path zipFile =  Paths.get(reader.readLine());
-            ZipFileManager zipFileManager = new ZipFileManager(zipFile);
-            System.out.println("Введите полный путь к файлу для архивации, включая имя файла");
-            Path source = Paths.get(reader.readLine());
-            zipFileManager.createZip(source);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите полный путь к файлу архива, включая имя файла");
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(reader.readLine()));
+
+        System.out.println("Введите полный путь к файлу для архивации, включая имя файла");
+        zipFileManager.createZip(Paths.get(reader.readLine()));
     }
 }
--- ZipFileManager.java
+++ ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,6 +14,17 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
-
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            byte[] buffer = new byte[8 * 1021];
+            int len;
+            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(source))) {
+                while ((len = zipInputStream.read(buffer)) > 0) {
+                    zipOutputStream.write(buffer, 0, len);
+                }
+            }
+            zipOutputStream.closeEntry();
+        }
     }
 }
