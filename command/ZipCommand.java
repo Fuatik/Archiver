76a82422874406e08c86bd95de6eaea0fa5d5919
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-02-22 21:45:22 +0100

    Added a ZipContentCommand

--- command/ZipCommand.java
+++ command/ZipCommand.java
@@ -6,9 +6,10 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-abstract public class ZipCommand implements Command{
+public abstract class ZipCommand implements Command {
+
     public ZipFileManager getZipFileManager() throws Exception {
-        ConsoleHelper.writeMessage("Введите полный путь файла архива");
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
         Path zipPath = Paths.get(ConsoleHelper.readString());
         return new ZipFileManager(zipPath);
     }
